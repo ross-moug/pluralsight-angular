cceeea454f087acff40dd3310c99f02b23c5bb9b
Ross Moug <ross.moug@gmail.com>
2018-09-19 13:09:04 +0100

    Add Child Component
    ...and pass input.

--- module 2 - components/ng-fundamentals/src/app/app.module.ts
+++ module 2 - components/ng-fundamentals/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events-list/events-list.component';
+import { EventThunbmailComponent } from './event-thunbmail/event-thunbmail.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThunbmailComponent
   ],
   imports: [
     BrowserModule
