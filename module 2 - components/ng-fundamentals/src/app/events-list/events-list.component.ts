d5e91c0acf5d0fa9d5e6bd4c2cf082421326cfea
Ross Moug <ross.moug@gmail.com>
2018-09-19 13:28:07 +0100

    Angular CSS Encapsulation

--- module 2 - components/ng-fundamentals/src/app/events-list/events-list.component.ts
+++ module 2 - components/ng-fundamentals/src/app/events-list/events-list.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-list',
-  templateUrl: './events-list.component.html'
+  templateUrl: './events-list.component.html',
+  styles: [`
+    .well div { color: blue; }
+  `]
 })
 export class EventsListComponent {
   event = {
