20e938bd5746e08eb645af784c1ffb55dc7e1a16
Ross Moug <ross.moug@gmail.com>
2018-09-20 13:27:52 +0100

    Handling null values safely

--- src/app/event-thumbnail/event-thumbnail.component.ts
+++ src/app/event-thumbnail/event-thumbnail.component.ts
@@ -7,20 +7,23 @@ import {
   selector: 'event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: £{{event.price}}</div>
+      <h2>{{event?.name}}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div>Time: {{event?.time}}</div>
+      <div>Price: £{{event?.price}}</div>
       <div>
-        <span>Location: {{event.location.address}}</span>
-        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <span>Location: {{event?.location?.address}}</span>
+        <span class="pad-left">{{event?.location?.city}}, {{event.location?.country}}</span>
+      </div>
+      <div>
+          Online URL: {{event?.onlineUrl}}
       </div>
     </div>
   `,
   styles: [`
     .pad-left { margin-left: 10px; }
     .well div { color: red; }
-    .thumbnail { min-height: 210px;}
+    .thumbnail { min-height: 240px;}
   `]
 })
 export class EventThumbnailComponent {
--- src/app/events-list/events-list.component.ts
+++ src/app/events-list/events-list.component.ts
@@ -101,6 +101,7 @@ export class EventsListComponent {
                 city: 'Amsterdam',
                 country: 'Netherlands'
             },
+            onlineUrl: 'http://ng-nl.org/',
             sessions: [
                 {
                     id: 1,
