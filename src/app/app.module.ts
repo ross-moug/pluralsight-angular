939729317177ee630922627a915301f5fad23e3e
Ross Moug <ross.moug@gmail.com>
2018-09-20 21:40:42 +0100

    Move components into new directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events-list/events-list.component';
-import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { EventsListComponent } from './components/events-list/events-list.component';
+import { EventThumbnailComponent } from './components/event-thumbnail/event-thumbnail.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
