170ade117b68eee5594c4e9b8e6ac69324699787
Ross Moug <ross.moug@gmail.com>
2018-10-16 19:51:50 +0100

    Further testing of services

--- ng-fundamentals/src/app/services/voter.service.spec.ts
+++ ng-fundamentals/src/app/services/voter.service.spec.ts
@@ -1,5 +1,5 @@
 import { VoterService } from "./voter.service";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Session } from "../models";
 import { of } from "rxjs";;
 
@@ -24,6 +24,45 @@ describe('VoterService', () => {
 
       expect(session.voters.length).toBe(2);
       expect(session.voters[1]).toBe('Claire');
+    });
+
+    it('should call http.delete with the right URL', () => {
+      mockHttp.delete.and.returnValue(of());
+      const session: Session = <Session>{
+        id: 6,
+        voters: ['Alice', 'Bob', 'Claire']
+      };
+
+      service.deleteVoter(3, session, 'Bob');
+
+      expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/Bob', jasmine.any(Object));
+    });
+  });
+
+  describe('addVoter', () => {
+    it('should add the voter to the list of voters', () => {
+      mockHttp.post.and.returnValue(of());
+      const session: Session = <Session>{
+        id: 6,
+        voters: ['Alice', 'Bob', 'Claire']
+      };
+
+      service.addVoter(3, session, 'Dave');
+
+      expect(session.voters.length).toBe(4);
+      expect(session.voters[3]).toBe('Dave');
+    });
+
+    it('should call http.post with the right URL', () => {
+      mockHttp.post.and.returnValue(of());
+      const session: Session = <Session>{
+        id: 6,
+        voters: ['Alice', 'Bob', 'Claire']
+      };
+
+      service.addVoter(3, session, 'Dave');
+
+      expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/Dave', {}, jasmine.any(Object));
     });
   });
 });
