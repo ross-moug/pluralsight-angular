412260e37ec5f53ed01dba99faaebfa96cc6c976
Ross Moug <ross.moug@gmail.com>
2018-10-16 13:02:51 +0100

    Add events to nav menu

--- ng-fundamentals/src/app/components/nav-bar/nav-bar.component.ts
+++ ng-fundamentals/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 import { AuthService } from '../../user/services/auth.service';
-import { Session } from '../../models';
+import { Session, Event } from '../../models';
 import { EventService } from '../../services';
 
 @Component({
@@ -8,18 +11,29 @@ import { EventService } from '../../services';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
   searchTerm: string = '';
   foundSessions: Session[] = [];
+  events: Event[] = [];
 
   constructor(
     public authService: AuthService,
     private eventService: EventService) {
-    }
+  }
+
+  ngOnInit(): void {
+    this.getEvents();
+  }
 
   searchSessions(searchTerm: string): void {
     this.eventService.searchSessions(searchTerm).subscribe(
       sessions => this.foundSessions = sessions
+    );
+  }
+
+  getEvents(): void {
+    this.eventService.getEvents().subscribe(
+      events => this.events = events
     );
   }
 }
