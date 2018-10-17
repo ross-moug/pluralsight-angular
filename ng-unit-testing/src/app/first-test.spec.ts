48be2390afa69f34603ea4fd8cd8c7c27a9095a2
Ross Moug <ross.moug@solarwinds.com>
2018-10-17 14:14:28 +0100

    Creating first test

---
+++ ng-unit-testing/src/app/first-test.spec.ts
@@ -0,0 +1,15 @@
+describe('my first test', () => {
+  let sut;
+
+  beforeEach(() => {
+    sut = {};
+  });
+
+  it('should be true if true', () => {
+    sut.a = false;
+
+    sut.a = true;
+
+    expect(sut.a).toBeTruthy();
+  });
+});
