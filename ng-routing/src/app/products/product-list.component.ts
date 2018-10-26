84ea791b63d26963f7eb3026631e8ca701985baa
Ross Moug <ross.moug@solarwinds.com>
2018-10-26 13:29:44 +0100

    Defining and reading query route parameters

--- ng-routing/src/app/products/product-list.component.ts
+++ ng-routing/src/app/products/product-list.component.ts
@@ -1,31 +1,44 @@
-import { Component, OnInit }  from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-    templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-    pageTitle: string = 'Product List';
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    showImage: boolean = false;
-    listFilter: string;
-    errorMessage: string;
+  pageTitle = 'Product List';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+  listFilter: string;
+  errorMessage: string;
 
-    products: IProduct[];
+  products: IProduct[];
 
-    constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private activatedRoute: ActivatedRoute) {
+  }
 
-    toggleImage(): void {
-        this.showImage = !this.showImage;
-    }
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 
-    ngOnInit(): void {
-        this.productService.getProducts()
-                .subscribe(products => this.products = products,
-                           error => this.errorMessage = <any>error);
-    }
+  ngOnInit(): void {
+    this.productService.getProducts()
+      .subscribe(products => this.products = products,
+        error => this.errorMessage = <any>error);
+
+    this.activatedRoute.queryParamMap.subscribe(
+      params => {
+        this.listFilter = params.get('filterBy') || '';
+        this.showImage = params.get('showImage') === 'true';
+      }
+    );
+  }
 }
