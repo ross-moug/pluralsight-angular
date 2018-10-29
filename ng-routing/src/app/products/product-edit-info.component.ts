73b137c7c0ac97cd43bcf5f4400e4aab3959fdab
Ross Moug <ross.moug@gmail.com>
2018-10-29 13:18:58 +0000

    Obtaining data for child routes

--- ng-routing/src/app/products/product-edit-info.component.ts
+++ ng-routing/src/app/products/product-edit-info.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { IProduct } from './product';
 
 @Component({
-    templateUrl: './product-edit-info.component.html'
+  templateUrl: './product-edit-info.component.html'
 })
 export class ProductEditInfoComponent implements OnInit {
-    @ViewChild(NgForm) productForm: NgForm;
+  @ViewChild(NgForm) productForm: NgForm;
 
-    errorMessage: string;
-    product = { id: 1, productName: 'test', productCode: 'test' };
+  errorMessage: string;
+  product: IProduct;
 
-    constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+    this.route.parent.params.subscribe(
+      data => {
+        this.product = data['product'];
+
+        if (this.productForm) {
+          this.productForm.resetForm();
+        }
+      }
+    );
+  }
 }
