e66ee83d0e7ee1401d42fa04d5415f728e1edcbf
Ross Moug <ross.moug@solarwinds.com>
2018-10-31 13:23:06 +0000

    Reacting to routing events

--- ng-routing/src/app/app.component.ts
+++ ng-routing/src/app/app.component.ts
@@ -1,5 +1,15 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  Event
+} from '@angular/router';
 
 import { AuthService } from './user/auth.service';
 
@@ -7,15 +17,34 @@ import { AuthService } from './user/auth.service';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   pageTitle = 'Acme Product Management';
+  loading = true;
 
   constructor(private authService: AuthService,
               private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.router.events.subscribe(
+      event => this.handleEvent(event)
+    );
+  }
+
   logOut(): void {
     this.authService.logout();
     this.router.navigateByUrl('/welcome');
+  }
+
+  private handleEvent(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError) {
+      this.loading = false;
+    }
   }
 }
