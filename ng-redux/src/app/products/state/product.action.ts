2c6206e5a2de26f3f8238a36058d6e84b28f7d51
Ross Moug <ross.moug@gmail.com>
2018-11-07 21:18:57 +0000

    Building action creators

--- ng-redux/src/app/products/state/product.action.ts
+++ ng-redux/src/app/products/state/product.action.ts
@@ -1,8 +1,35 @@
+import { Product } from './../product';
 import { Action } from '@ngrx/store';
 
-export const TOGGLE_PRODUCT_CODE: string = 'TOGGLE_PRODUCT_CODE';
+export enum ProductActionType {
+  ToggleActionCode = '[Product] Toggle Product Code',
+  SetCurrentProduct = '[Product] Set Current Product',
+  ClearCurrentProduct = '[Product] Clear Current Product',
+  InitialiseCurrentProduct = '[Product] Initialise Current Product',
+}
 
 export class ToggleProductCodeAction implements Action {
-  type: string =  TOGGLE_PRODUCT_CODE;
-  payload: boolean;
+  readonly type: string = ProductActionType.ToggleActionCode;
+
+  constructor(public payload: boolean) {}
 }
+
+export class SetCurrentProductAction implements Action {
+  readonly type: string = ProductActionType.SetCurrentProduct;
+
+  constructor(public payload: Product) {}
+}
+
+export class ClearCurrentProductAction implements Action {
+  readonly type: string = ProductActionType.ClearCurrentProduct;
+}
+
+export class InitialiseCurrentProductAction implements Action {
+  readonly type: string = ProductActionType.InitialiseCurrentProduct;
+}
+
+export type ProductActions = ToggleProductCodeAction
+  | SetCurrentProductAction
+  | ClearCurrentProductAction
+  | InitialiseCurrentProductAction
+;
