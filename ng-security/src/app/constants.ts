e0ebde0b302d17ee4daef09ccf1b79ac2f1699c2
Ross Moug <ross.moug@solarwinds.com>
2018-11-15 13:43:01 +0000

    Revert "Configuring oidc-client for Auth0 STS"
    This reverts commit 2ef5d2c

--- ng-security/src/app/constants.ts
+++ ng-security/src/app/constants.ts
@@ -1,13 +1,8 @@
 export class Constants {
-
   public static apiRoot: string = 'https://securingangularappscourse-api.azurewebsites.net/api/';
   public static clientRoot: string = 'http://localhost:4200/';
+  public static stsAuthority = "https://securingangularappscourse-sts.azurewebsites.net/";
+  public static clientId = 'spa-client';
 
-  /* IdentityServer-specific config */
-  // public static stsAuthority: string = "https://securingangularappscourse-sts.azurewebsites.net/";
-  // public static clientId: string = 'spa-client';
-
-  /* Auth0-specific config */
-  public static stsAuthority: string = "https://softinsight.auth0.com/";
-  public static clientId: string = 'FVZYzaiuyFYR4bxPTtSriqNLgAE69Btn';
+  // public static apiRoot = 'http://localhost:2112/api/';
 }
