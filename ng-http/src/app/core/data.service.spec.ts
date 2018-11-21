183dbd4bdc798423568bb3e3d4c47650016637f1
Ross Moug <ross.moug@gmail.com>
2018-11-21 22:04:11 +0000

    Testing HTTP errors

--- ng-http/src/app/core/data.service.spec.ts
+++ ng-http/src/app/core/data.service.spec.ts
@@ -1,3 +1,4 @@
+import { BookTrackerError } from './../models/bookTrackerError';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 
@@ -9,7 +10,8 @@ describe('DataService', () => {
     { bookID: 1, title: 'Harry Potter and the Philosopher\'s Stone', author: 'J. K. Rowling', publicationYear: 1997 },
     { bookID: 2, title: 'Harry Potter and the Chamber of Secrets', author: 'J. K. Rowling', publicationYear: 1998 },
     { bookID: 3, title: 'Harry Potter and the Prisoner of Azkaban', author: 'J. K. Rowling', publicationYear: 1999 },
-  ]
+  ];
+
   let service: DataService;
   let httpTestingController: HttpTestingController;
 
@@ -27,6 +29,10 @@ describe('DataService', () => {
     httpTestingController = TestBed.get(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
   it('should GET all books', () => {
     service.getAllBooks()
       .subscribe(
@@ -38,7 +44,23 @@ describe('DataService', () => {
     expect(mockRequest.request.method).toEqual('GET');
 
     mockRequest.flush(books);
+  });
 
-    httpTestingController.verify();
+  it('should convert error to BookTrackerError', () => {
+    service.getAllBooks()
+      .subscribe(
+        () => fail('should have been an error'),
+        (error: BookTrackerError) => {
+          expect(error.errorNumber).toEqual(100);
+          expect(error.friendlyMessage).toEqual('An error occurred retrieving data');
+        }
+      );
+
+    const mockRequest: TestRequest = httpTestingController.expectOne('/api/books');
+
+    mockRequest.flush(books, {
+      status: 500,
+      statusText: 'Server Error',
+    });
   });
 });
