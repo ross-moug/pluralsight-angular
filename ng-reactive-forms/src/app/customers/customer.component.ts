3a4cc80a1eeef1c00dd36b7e586e2bd1d454726f
Ross Moug <ross.moug@gmail.com>
2018-12-03 22:19:41 +0000

    setValue and patchValue

--- ng-reactive-forms/src/app/customers/customer.component.ts
+++ ng-reactive-forms/src/app/customers/customer.component.ts
@@ -27,4 +27,12 @@ export class CustomerComponent implements OnInit {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: "Bob",
+      lastName: "Dylan",
+      email: "bob@dylan.com",
+    });
+  }
 }
